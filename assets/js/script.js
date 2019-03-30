a005533a2e4ce9491d71e21d1708d4e5a33e69d9
Ralph Enciso <enciso.ralph@gmail.com>
2019-03-30 21:05:28 +0800

    custom scrolls

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,48 @@ var vis = document.querySelector(".vision");
 
 
 
-abt.onmouseleave = function(){
-    this.scrollTop = 0;
+
+
+// custom scrollbar
+abt.onmouseenter = customScroll;
+mis.onmouseenter = customScroll;
+vis.onmouseenter = customScroll;
+
+
+function customScroll(e){
+
+    var elmnt = e.target
+    var cscroll = e.target.querySelector(".Scroll");
+    var hdiff = elmnt.scrollHeight - elmnt.offsetHeight; 
+    var sHeight =  elmnt.offsetHeight * elmnt.offsetHeight / elmnt.scrollHeight; 
+    var vdiff = elmnt.offsetHeight - sHeight;
+    sHeight -= cscroll.offsetTop;
+
+    cscroll.style.height = sHeight.toString() + 'px';
+
+    console.log(this.scrollHeight);
+
+
+        elmnt.onscroll = function(){
+            var sratio = this.scrollTop / hdiff;
+            
+            var tvalue = sratio * (hdiff + vdiff);
+            
+            if(tvalue >= (hdiff + vdiff)) {return};
+
+            cscroll.style.transform = 'translateY(' + tvalue + 'px)'
+        }
+
+
+        elmnt.onmouseleave = function() {
+            this.scrollTop = 0;
+            cscroll.style.height = 0;
+            cscroll.style.transform = 'translateY(0)';
+        }
+
 }
 
-mis.onmouseleave = function(){
-    this.scrollTop = 0;
-}
 
-vis.onmouseleave = function(){
-    this.scrollTop = 0;
-}
 
 
 
